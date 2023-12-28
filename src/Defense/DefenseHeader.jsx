42108ee5fb716386096e0403905812d747f0a5a2
Cody Farrington <fcody1520@gmail.com>
2023-12-28 13:08:58 -0700

    add framework for defense table and header

---
+++ src/Defense/DefenseHeader.jsx
@@ -0,0 +1,17 @@
+export default function DefenseHeader(){
+    return (
+        <>
+            <thead>
+                <tr>
+                    <th className={"widecolumn"}></th>
+                    <th className={"widecolumn"}>Player Name</th>
+                    <th className={"skinnycolumn"}>Flag Pulls</th>
+                    <th className={"skinnycolumn"}>Sacks</th>
+                    <th className={"skinnycolumn"}>Interceptions</th>
+                    <th className={"skinnycolumn"}>Knockdowns</th>
+                    <th className={"skinnycolumn"}>Defensive TD's</th>
+                </tr>
+            </thead>
+        </>
+    )
+}
